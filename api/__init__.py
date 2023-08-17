d8c07a67ce4fffe3bfab4315144aeffccab27ebe
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-08-17 15:46:18 +0630

    add base code

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,13 @@
 from flask import Flask
+from flask_restful import Api
+from config import api_config
+
+
+app = Flask(__name__)
+app.config.from_object(api_config['development'])  # Change to 'production' for production
+api = Api(app)
+
+register_routes(api)
+
+def create_app():
+    return app
