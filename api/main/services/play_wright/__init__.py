747c27f37af7afea295f5bb594ce9b810f5d6b90
Kyaw Thant Zin <kyawthantzamt@gmail.com>
2023-08-21 17:26:15 +0630

    Update

--- api/main/services/play_wright/__init__.py
+++ api/main/services/play_wright/__init__.py
@@ -13,7 +13,8 @@ class PlaywrightService:
             self._start_browser() # launch the browser
 
     def _start_browser(self):
-        PlaywrightService._browser = sync_playwright().start().chromium.launch_persistent_context(
+        playwright = sync_playwright().start()
+        PlaywrightService._browser = playwright.chromium.launch_persistent_context(
             executable_path=self._browserPath, 
             headless=False,
             user_data_dir=self._usr_dir
@@ -21,8 +22,9 @@ class PlaywrightService:
         print('start the browser')
 
     def get_browser(self):
-        return PlaywrightService._browser
+        if not PlaywrightService._browser:
+            return PlaywrightService._browser #return the browser
+        else:
+            self._start_browser() # launch the browser
 
-    def get_page_content(self):
-        print('open a new page')
     
--- api/main/services/social_media/ig/__init__.py
+++ api/main/services/social_media/ig/__init__.py
@@ -1,9 +1,15 @@
-from ...playwright import PlaywrightService
+from ...play_wright import PlaywrightService
 
 class IG:
 
-    def get_profile(self, username):
+    _ig_url = ''
+    _ig_usr_url = ''
+    _ig_post_limit = 12
+
+    def __init__(self):
         playwright_service = PlaywrightService() 
-        playwright_service.get_browser()  
+
+    def get_profile(self, username):
+        browser = PlaywrightService().get_browser()  
         print('get the profile')
         return username
